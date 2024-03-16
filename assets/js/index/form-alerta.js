f3f332a23066268fe67feab2b03f0307e73c68e8
Fellipe Reis <reisfellipe11@gmail.com>
2024-03-16 02:33:41 -0300

    fix alert

--- assets/js/index/form-alerta.js
+++ assets/js/index/form-alerta.js
@@ -2,5 +2,13 @@ const alerta = document.querySelector('#alerta');
 const botaForm = document.querySelector('#_form_1_submit');
 
 botaForm.addEventListener('click', () => {
-  alerta.classList.add('mostrar');
-})
+  setTimeout(() => {
+    alerta.classList.add('mostrar');
+  }, 1000);
+});
+
+function hiddenElement() {
+  alerta.style.display = 'none'; // Corrigido para ocultar o elemento alerta
+}
+
+setTimeout(hiddenElement, 7000);
