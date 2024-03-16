774781cf1c0200cfd682ba8c48b4a289f1d13c51
Fellipe Reis <reisfellipe11@gmail.com>
2024-03-16 02:41:06 -0300

    hidden alert

--- assets/js/index/form-alerta.js
+++ assets/js/index/form-alerta.js
@@ -2,10 +2,8 @@ const alerta = document.querySelector('#alerta');
 const botaForm = document.querySelector('#_form_1_submit');
 
 botaForm.addEventListener('click', () => {
-  setTimeout(() => {
     alerta.classList.add('mostrar');
-  }, 1000);
-});
+  });
 
 function hiddenElement() {
   alerta.style.display = 'none'; // Corrigido para ocultar o elemento alerta
